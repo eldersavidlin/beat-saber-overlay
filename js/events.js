c3c743c7cb2b62b027594381a11aa79f94e7ec03
eldersavidlin <68166134+eldersavidlin@users.noreply.github.com>
2020-07-11 17:17:39 -0400

    Modified to remove performance

--- js/events.js
+++ js/events.js
@@ -4,22 +4,22 @@ const events = {
 
 		if (data.beatmap && data.performance) {
 			ui.beatmap(data.beatmap, time);
-			ui.performance(data.performance);
+//			ui.performance(data.performance);
 			ui.show();
 		}
 	},
 
 	songStart(data, time) {
 		ui.beatmap(data.beatmap, time);
-		ui.performance(data.performance);
+//		ui.performance(data.performance);
 		ui.show();
 	},
 
-	noteCut(data) { ui.performance(data.performance); },
-	noteFullyCut(data) { ui.performance(data.performance); },
-	obstacleEnter(data) { ui.performance(data.performance); },
-	noteMissed(data) { ui.performance(data.performance); },
-	bombCut(data) { ui.performance(data.performance); },
+//	noteCut(data) { ui.performance(data.performance); },
+//	noteFullyCut(data) { ui.performance(data.performance); },
+//	obstacleEnter(data) { ui.performance(data.performance); },
+//	noteMissed(data) { ui.performance(data.performance); },
+//	bombCut(data) { ui.performance(data.performance); },
 
 	pause(data, time) {
 		ui.timer.pause(data.beatmap.paused + (Date.now() - time));
